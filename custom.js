213ecfb6c20e2bcad5946c425453449090ca93ac
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2018-11-24 13:40:39 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,31 +1,25 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var mutationTest = true;
     console.log('DomContent Loaded')
     
 const holder = $('#holder');
     
     var observer = new MutationObserver(function(mutationsList, observer) {
-    console.log('Mutation Loaded')
-    
         $(mutationsList).each(function(index, item){
-			var mutationTest = true;
             if (item.type === 'childList'){
 
                 if (item.addedNodes.length > 0){
-                    console.log('new div is being added! ' + item.name );
+                    console.log('new div is being added! ' + item.tagName );
                 }
 
             }
         });
     });
 
-    if (mutationTest){
     // Start observing the target node for configured mutations
     observer.observe( holder[0], { 
         attributes: true, 
         childList: true, 
         subtree: true 
     });	
-    };
 });
