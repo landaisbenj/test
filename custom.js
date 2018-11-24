2f1caa26a5d03d832d72967aa8696006f5cdccf3
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2018-11-24 12:44:45 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,26 +1,36 @@
 $( document ).ready(function() {
 
+  if ($('#main-view div.row').next().length != 0 ){
 console.log('Ready ok')
+		}
+  
 
 });
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  if ($('#main-view div.row').next().length != 0 ){
 console.log('domcontent ok')
+  }
 });
 
 $(document).ajaxSuccess(function (event, xhr, settings) {
 
+  if ($('#main-view div.row').next().length != 0 ){
 console.log('ajax success ok')
+  }
 });
 
 $(document).ajaxComplete(function (event, xhr, settings) {
 
+  if ($('#main-view div.row').next().length != 0 ){
 console.log('ajax complete ok')
+  }
 });
 
 $( window ).load(function() {
 
+  if ($('#main-view div.row').next().length != 0 ){
 console.log('windows load ok')
-
+  }
 });
