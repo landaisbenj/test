50d4cef6cb5c98229a64c54ce8295e8d0d4cd2a6
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2018-11-24 12:48:30 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,34 +1,26 @@
-  if ($('#main-view div.row').next().length != 0 ){
-console.log('sans rien ok')
-  }
 
 $( document ).ready(function() {
 
-  if ($('#main-view div.row').next().length != 0 ){
 console.log('Ready ok')
-		}
-  
+		
 
 });
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  if ($('#main-view div.row').next().length != 0 ){
 console.log('domcontent ok')
-  }
+  
 });
 
 $(document).ajaxSuccess(function (event, xhr, settings) {
 
-  if ($('#main-view div.row').next().length != 0 ){
 console.log('ajax success ok')
-  }
+ 
 });
 
 $(document).ajaxComplete(function (event, xhr, settings) {
 
-  if ($('#main-view div.row').next().length != 0 ){
 console.log('ajax complete ok')
-  }
+  
 });
 
